a19569ab0027d9538d9633f3e62028ceec6944a7
BERNARDO DE OLIVEIRA DOS SANTOS <85496116+Bernardo-OS@users.noreply.github.com>
2026-07-02 16:32:35 -0300

    Begin to add time game limit

--- jogo_logico/main_jogo.py
+++ jogo_logico/main_jogo.py
@@ -12,7 +12,7 @@
 
 import time
 
-tempo_limite = 60  # segundos
+tempo_limite = 180  # segundos
 
 
 
@@ -24,25 +24,39 @@ print("Levando em consideração que as proposições devem dar valor verdadeiro
 print("O valor positivo significa no caso desconectá-lo do circuito, você deve preencher em sequência de acordo com a solução das proposições os fios que cortar corretamente.")
 input("Pressione Enter para começar o jogo")
 print("As pistas são as seguintes:")
-print("1) R → B \n2) ~(R ∧ G) \n3) B ∨ G \n4) Y → B \n5) R")
+print("(1) R → B \n (2) ~(R ∧ G) \n(3) B ∨ G \n(4) Y → B \n(5) R")
 sequencia_correta = ["R", "B", "G", "Y"]  # Sequência correta de fios a cortar
 inicio = time.time()
 
-
-
-
-while True:
-    tempo_decorrido = time.time() - inicio
+"""
+tempo_decorrido = time.time() - inicio
     if tempo_decorrido >= tempo_limite:
         print("Tempo esgotado! A bomba explodiu!")
         break
+"""
 
-    resposta = input("Digite a sequência de fios a cortar (ex: RBGY): ").upper()
+while time.time() - inicio < tempo_limite:
+    tempo_restante = int(tempo_limite - (time.time() - inicio))
+    i = 0
+    resposta = input("Digite o próximo fio a cortar (r, b, g, y, w, k): ").upper()
+    i+=1
 
-    if i > sequencia_correta.len:
+    if resposta != sequencia_correta[i]:
+        print("Fio incorreto!")
+        break
+
+    if i == len(sequencia_correta) and sequencia_correta[i] == resposta:
         print("Parabéns! Você desarmou a bomba com sucesso!")
         break
     else:
         print("Sequência incorreta! Tente novamente.")
-tempo_restante = int(tempo_limite - (time.time() - inicio))
-print(f"Tempo restante: {tempo_restante}s")
+        
+if tempo_restante <= 0:
+    print("Tempo esgotado!")
+print("Bomba explodiu! A sequência correta era:", sequencia_correta)
+
+
+
+    '''
+    print(f"Tempo restante: {tempo_restante}s")
+    '''
